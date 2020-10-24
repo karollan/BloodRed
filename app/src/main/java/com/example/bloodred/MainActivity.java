b09dbe2d02071cfdce14b2cc4abcfcdebc7ffdfe
karollan <topireaves@gmail.com>
2020-10-24 15:18:37 +0200

    Game loop

--- app/src/main/java/com/example/bloodred/MainActivity.java
+++ app/src/main/java/com/example/bloodred/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.bloodred;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.Window;
+import android.view.WindowManager;
 import android.os.Bundle;
+
+/*
+* MainActivity application start point
+ */
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Window set to fullscreen, hide the status bar
+        Window window = getWindow();
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Content set view to game, objects in Game class can be rendered to the screen
+        setContentView(new Game(this));
     }
 }

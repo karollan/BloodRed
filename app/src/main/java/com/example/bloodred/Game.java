0100d65ff509a6684ba886440c6cd697eb0c2833
karollan <topireaves@gmail.com>
2020-10-24 16:44:35 +0200

    Syringe class

--- app/src/main/java/com/example/bloodred/Game.java
+++ app/src/main/java/com/example/bloodred/Game.java
@@ -3,6 +3,7 @@ package com.example.bloodred;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -18,7 +19,8 @@ import androidx.core.content.ContextCompat;
 
 public class Game extends SurfaceView implements SurfaceHolder.Callback {
     private GameLoop gameLoop;
-    private Context context;
+
+    private final Syringe syringe;
 
     public Game(Context context) {
         super(context);
@@ -27,10 +29,28 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         SurfaceHolder surfaceHolder = getHolder();
         surfaceHolder.addCallback(this);
 
-        this.context = context;
         gameLoop = new GameLoop(this, surfaceHolder);
 
+        // Initialize syringe
+        syringe = new Syringe(getContext(), 500, 500, 30);
+
         setFocusable(true);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        //Handle touch event actions
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                syringe.setPosition((double)event.getX(), (double)event.getY());
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                syringe.setPosition((double)event.getX(), (double)event.getY());
+                return true;
+        }
+
+        return super.onTouchEvent(event);
     }
 
     @Override
@@ -53,12 +73,14 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
         super.draw(canvas);
         drawUPS(canvas);
         drawFPS(canvas);
+
+        syringe.draw(canvas);
     }
 
     public void drawUPS(Canvas canvas) {
         String averageUPS = Double.toString(gameLoop.getAverageUPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.magenta);
+        int color = ContextCompat.getColor(getContext(), R.color.magenta);
         paint.setColor(color);
         paint.setTextSize(50);
         canvas.drawText( "UPS: " + averageUPS,  100,  100, paint);
@@ -67,7 +89,7 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
     public void drawFPS(Canvas canvas) {
         String averageFPS = Double.toString(gameLoop.getAverageFPS());
         Paint paint = new Paint();
-        int color = ContextCompat.getColor(context, R.color.magenta);
+        int color = ContextCompat.getColor(getContext(), R.color.magenta);
         paint.setColor(color);
         paint.setTextSize(50);
         canvas.drawText( "FPS: " + averageFPS,  100,  200, paint);
@@ -75,5 +97,6 @@ public class Game extends SurfaceView implements SurfaceHolder.Callback {
 
     public void update() {
         // Update game state
+        syringe.update();
     }
 }
